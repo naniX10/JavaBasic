0d006d9de96fd5548e237296c481ea88d5203dd6
nanix10 <virtuoso0201@gmail.com>
2021-04-01 17:56:14 +0900

    성적처리프로그램v6 사원관리프로그램v1

---
+++ src/KTY/basic/lab/EmployeeService.java
@@ -0,0 +1,21 @@
+package kty.basic.lab;
+
+    // 대충 입력 출력 조회?
+
+public interface EmployeeService {
+
+
+    void displaymenu();
+
+    void newEmployee();
+
+    void showEmployee();
+
+    void showOneEmployee();
+
+    void modifyEmployee();
+
+    void removeEmployee();
+
+    void makeExtInfo(EmployeeVO emp);
+}
